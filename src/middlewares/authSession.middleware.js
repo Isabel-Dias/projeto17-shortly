d86c1d9bbdec044807711cfd58c16f2b195aeaf9
Daniel <danielbellinijf21@gmail.com>
2023-11-06 11:02:34 -0300

    refix: authorization middleware

--- src/middlewares/authSession.middleware.js
+++ src/middlewares/authSession.middleware.js
@@ -4,7 +4,9 @@ async function AuthSession(req, res, next) {
 
     const bearerToken = req.headers.authorization;
     let token = bearerToken?.replace('Bearer ', '');
-
+    
+    token = token.replace('"', "")
+    token = token.replace('"', "")
     
     if (!token) {
         return res.status(401).send({ message: "Não autorizado, token não recebido" });
@@ -13,41 +15,33 @@ async function AuthSession(req, res, next) {
     try {
 
         const session = await db.query(
-            `SELECT * 
-            FROM sessions`
+            `SELECT * FROM sessions
+            WHERE "token" = $1`, [token]
         )
-            
-        let resultSession = session.rows.map((entry) => {
-            if (token == `"${entry.token}"`) {
-                return entry;
-            }
-        })
 
-        resultSession = resultSession[0];
-
-        if (!resultSession) {
+        if (session.rowCount == 0) {
             return res.status(401).send({ message: "Não autorizado, sessão não cadastrada" });
         }
 
-        const id = resultSession.user_id
+        const id = session.rows[0].user_id
 
         const user = await db.query(
             `SELECT * 
             FROM users
             WHERE id = $1` , [id]
-            )
+        )
 
-            if(user.rowCount == 0){
-                return res.status(401).send({message: "Usuário não existe!"});
-            }
+        if(user.rowCount == 0){
+            return res.status(401).send({message: "Usuário não existe!"});
+        }
 
-            res.locals.user = {id}
+        res.locals.user = {id}
 
-            next();
+        next();
 
     } catch (error) {
         console.log(error);
-        return res.sendStatus(500);
+        return res.status(500).send(error);
     }
 }
 
