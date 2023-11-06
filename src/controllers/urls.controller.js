d98d725a5d79fa7117a9cdc8028e65a32b336b2e
Daniel <danielbellinijf21@gmail.com>
2023-11-05 21:33:56 -0300

    feat: open url

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -59,4 +59,35 @@ export async function getOneUrl(req, res) {
     }
 }
 
+export async function openUrl(req, res) {
+    try {
+       const { shortUrl }  = req.params;
+
+       const urlData = await db.query(`
+       SELECT *
+       FROM urls
+       WHERE short_url = $1`, [shortUrl]
+       )
+
+       if(urlData.rowCount == 0) {
+        return res.status(404).send({message: "Essa shortURL não existe!"})
+       }
+
+       const increasedCount = Number(urlData.rows[0].views) + 1 
+       const { id, url } = urlData.rows[0]
+
+       await db.query(
+        `UPDATE urls 
+        SET views = $1
+        WHERE id = $2`, [increasedCount, id]
+       )
+
+       return res.redirect(url)
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error)
+    }
+}
+
 
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import authSession from "../middlewares/authSession.middleware.js"
-import { getOneUrl, postUrl } from "../controllers/urls.controller.js";
+import { getOneUrl, openUrl, postUrl } from "../controllers/urls.controller.js";
 
 const router = Router();
 
 router.post("/urls/shorten", authSession, postUrl);
 router.get("/urls/:id", getOneUrl);
+router.get("/urls/open/:shortUrl", openUrl);
 
 
 export default router;
