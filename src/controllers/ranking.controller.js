a9c15dc0a04e7fbb617deb71fb3630a8f16e4cb7
Daniel <danielbellinijf21@gmail.com>
2023-11-06 00:45:38 -0300

    feat: get rankings

---
+++ src/controllers/ranking.controller.js
@@ -0,0 +1,27 @@
+import db from "../database/database.connection.js"
+
+export default async function getRankings (req, res) {
+    try {
+
+        const ranking = await db.query(
+          `SELECT
+                users.id,
+                users.name,
+                COUNT(urls.id) AS "linksCount",
+                COALESCE(SUM(urls.views), 0) AS "visitCount"
+            FROM users
+            LEFT JOIN urls 
+            ON users.id = urls.user_id
+            GROUP BY users.id
+            ORDER BY "visitCount" DESC
+            LIMIT 10`  
+        )
+
+        console.log(ranking.rows);
+
+        return res.status(200).send(ranking.rows)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error)
+    }
+}
